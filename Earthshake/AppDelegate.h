52c76e012fa83e8e5fcfa5f748c19ae44d708219
andreypaunov <andrey.paunov@hotmail.com>
2016-03-24 15:08:21 -0400

    Initial Commit

---
+++ Earthshake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Earthshake
+//
+//  Created by Andrey Paunov on 2016-03-24.
+//  Copyright © 2016 Andrey Paunov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
